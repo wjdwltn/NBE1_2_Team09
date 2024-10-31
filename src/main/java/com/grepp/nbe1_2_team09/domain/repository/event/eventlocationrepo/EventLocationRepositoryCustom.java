70d5e8c03d6425430db6094424fb97bb87bde495
wjdwltn <jlsowltn2000@naver.com>
2024-11-01 00:28:31 +0900

    Refactor: findByEventAndDate QueryDsl로 변경

---
+++ src/main/java/com/grepp/nbe1_2_team09/domain/repository/event/eventlocationrepo/EventLocationRepositoryCustom.java
@@ -0,0 +1,12 @@
+package com.grepp.nbe1_2_team09.domain.repository.event.eventlocationrepo;
+
+import com.grepp.nbe1_2_team09.domain.entity.event.Event;
+import com.grepp.nbe1_2_team09.domain.entity.event.EventLocation;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface EventLocationRepositoryCustom {
+    List<EventLocation> findByEventAndDate(Event event, LocalDate date);
+
+}
---
+++ src/main/java/com/grepp/nbe1_2_team09/domain/repository/event/eventlocationrepo/EventLocationRepositoryImpl.java
@@ -0,0 +1,43 @@
+package com.grepp.nbe1_2_team09.domain.repository.event.eventlocationrepo;
+
+import com.grepp.nbe1_2_team09.domain.entity.event.Event;
+import com.grepp.nbe1_2_team09.domain.entity.event.EventLocation;
+import com.grepp.nbe1_2_team09.domain.entity.event.QEventLocation;
+import com.querydsl.core.types.ConstantImpl;
+import com.querydsl.core.types.dsl.DateTemplate;
+import com.querydsl.core.types.dsl.Expressions;
+import com.querydsl.core.types.dsl.StringTemplate;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@RequiredArgsConstructor
+@Repository
+public class EventLocationRepositoryImpl implements EventLocationRepositoryCustom{
+
+    private final JPAQueryFactory queryFactory;
+
+    @Override
+    public List<EventLocation> findByEventAndDate(Event event, LocalDate date) {
+        QEventLocation eventLocation = QEventLocation.eventLocation;
+
+        // visitStartTime의 날짜 부분을 "yyyy-MM-dd" 형식으로 포맷팅
+        StringTemplate formattedDate = Expressions.stringTemplate(
+                "DATE_FORMAT({0}, {1})",
+                eventLocation.visitStartTime,
+                ConstantImpl.create("%Y-%m-%d")
+        );
+
+        return queryFactory.selectFrom(eventLocation)
+                .where(eventLocation.event.eq(event)
+                        .and(formattedDate.eq(date.toString()))) // LocalDate를 문자열로 변환하여 비교
+                .orderBy(eventLocation.visitStartTime.asc())
+                .fetch();
+    }
+}
