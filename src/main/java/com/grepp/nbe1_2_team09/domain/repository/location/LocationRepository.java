2de65d40842fccadec15f2c9aa989debc811c1d8
wjdwltn <jlsowltn2000@naver.com>
2024-10-31 11:20:26 +0900

    Refactor: 캐시를 사용한 장소 중복 저장 관리

--- src/main/java/com/grepp/nbe1_2_team09/domain/repository/location/LocationRepository.java
+++ src/main/java/com/grepp/nbe1_2_team09/domain/repository/location/LocationRepository.java
@@ -3,6 +3,8 @@ package com.grepp.nbe1_2_team09.domain.repository.location;
 import com.grepp.nbe1_2_team09.domain.entity.Location;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface LocationRepository extends JpaRepository<Location, Long> {
+import java.util.Optional;
 
+public interface LocationRepository extends JpaRepository<Location, Long> {
+    Optional<Location> findByPlaceId(String placeId);
 }
